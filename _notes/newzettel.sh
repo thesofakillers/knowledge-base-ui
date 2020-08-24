428525f4969a8ab8476bb7f1d8fd66edaaef7278
Giulio Starace <giulio.starace@gmail.com>
2020-08-24 17:21:32 +0200

    git subrepo pull _notes
    subrepo:
      subdir:   "_notes"
      merged:   "4cd70be"
    upstream:
      origin:   "git@github.com:thesofakillers/knowledge-base.git"
      branch:   "master"
      commit:   "4cd70be"
    git-subrepo:
      version:  "0.4.1"
      origin:   "???"
      commit:   "???"

---
+++ _notes/newzettel.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+currdate=`date -u +"%Y-%m-%d %H:%M:%S"`
+iddate=`date -u +"%Y-%m-%dt%H-%M-%Sz"`
+
+touch $iddate.md
+
+cat <<EOF > ./$iddate.md 
+---
+date: $currdate
+title: TITLE 
+id: $iddate
+---
+
+Write
+EOF
+
+
+vim '+call cursor(3,8)' ./$iddate.md 
